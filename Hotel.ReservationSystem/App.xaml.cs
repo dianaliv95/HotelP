bdcd89ba72becef2101a16c4272510b0c52d1fb3
dianaliv95 <dianaliv95@gmail.com>
2024-12-28 00:54:52 +0100

    powizania projektow miedzy soba

---
+++ Hotel.ReservationSystem/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Hotel.ReservationSystem
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Hotel.ReservationSystem/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Hotel.ReservationSystem
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
