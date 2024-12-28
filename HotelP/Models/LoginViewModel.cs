c4af8a1116a52df13bb834f61509b7f678611199
dianaliv95 <dianaliv95@gmail.com>
2024-12-28 01:15:31 +0100

    usuwane

---
+++ HotelP/Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hotel.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Pole e-mail jest wymagane.")]
+        [EmailAddress(ErrorMessage = "Wprowadź poprawny adres e-mail.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Pole hasło jest wymagane.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Zapamiętaj mnie")]
+        public bool RememberMe { get; set; }
+    }
+}
